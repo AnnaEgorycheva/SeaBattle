fb1e266223f9f63a48483c901084bd5168942876
Violet022 <vita.verkhulevskaya@mail.ru>
2022-06-11 18:46:44 +0300

    Добавлен алгоритм расстановки кораблей "Диагонали"
    Идейно алгоритм "Диагонали "похож на алгоритм "Произвольное расположение кораблей" за исключением того, что кроме общих условий расстановки кораблей необходимо также проверять, что никакая из палуб корабля не будет находится на какой-либо из двух диагоналей.
    Чтобы избежать дублирования тел вызываемых функций была предпринята попытка вызывать, в зависимости от выбранного алгоритма, различные функции проверки, но в таком случае, при данной реализации, это приводит к переполнению стека вызовов, поэтому, если проблема не будет решена, необходимо будет проводить проверку по диагоналям перед фиксированием расположения корабля и если она будет неуспешно, то снова получать начальные координаты и направление расположения и проверять валидность расположения по общим условиям.

--- js/ships_location_algorithms.js
+++ js/ships_location_algorithms.js
@@ -1,10 +1,15 @@
-//field = [...Array(10)].map(() => Array(10).fill(0));
+let SHIP_LOCATION_ALGORITHMS = {
+    random_ship_location_algoritm: 0,
+    diagonals_ship_location_algorithm: 1
+}
+let chosenAlgorithm; 
+let field = [...Array(10)].map(() => Array(10).fill(0));
 
 function getRandomNumber(n) {
     return Math.floor(Math.random() * (n + 1));
 } 
 
-function checkShipLocation(x, y, kx, ky, decks, playerField) {
+function checkShipLocationForRandomAlgoritm(x, y, kx, ky, decks, playerField) {
     let fromX, toX, fromY, toY;
 
 	fromX = (x == 0) ? x : x - 1;
@@ -32,7 +37,7 @@ function checkShipLocation(x, y, kx, ky, decks, playerField) {
 	return true;
 }
 
-function getStartDeckCoord(decksNum, playerField) {
+function getStartDeckCoordForRandomAlgoritm(decksNum, playerField) {
     let x, y;
     let kx = getRandomNumber(1); 
     let ky = (kx == 0) ? 1 : 0;
@@ -45,10 +50,10 @@ function getStartDeckCoord(decksNum, playerField) {
         y = getRandomNumber(9);
 	}
 
-    let result = checkShipLocation(x, y, kx, ky, decksNum, playerField);
+    let result = checkShipLocationForRandomAlgoritm(x, y, kx, ky, decksNum, playerField);
 
 	if (!result) 
-        return getStartDeckCoord(decksNum, playerField);
+        return getStartDeckCoordForRandomAlgoritm(decksNum, playerField);
     return {x, y, kx, ky}; // координаты первой палубы корабля, а также информацию о направлении
 }
 
@@ -56,7 +61,7 @@ function randomLocationShips(playerField, playerShips) {
     for (let ship of playerShips) {
 		let decks = ship.numOfDecks;
 
-		let shipInfo = getStartDeckCoord(decks, playerField);
+		let shipInfo = getStartDeckCoordForRandomAlgoritm(decks, playerField);
         let k = 0;
         for (let deck of ship.decks) {
             let x = shipInfo.x + k * shipInfo.kx;
@@ -69,8 +74,109 @@ function randomLocationShips(playerField, playerShips) {
     }
 }
 
-let enemy = new Enemy(10);
-randomLocationShips(enemy.playerField, enemy.ships);
+
+
+
+
+
+
+function checkShipLocationForDiagonalAlgoritm(x, y, kx, ky, decks) {
+    let fromX, toX, fromY, toY;
+
+	fromX = (x == 0) ? x : x - 1;
+    if (kx == 1)
+        toX = (x + kx * decks == 10) ? x + kx * decks : x + kx * decks + 1;
+    else 
+        toX = (x == 9) ? x + 1 : x + 2;
+
+    fromY = (y == 0) ? y : y - 1;
+    if (ky == 1)
+        toY = (y + ky * decks == 10) ? y + ky * decks : y + ky * decks + 1;
+    else
+        toY = (y == 9) ? y + 1 : y + 2;
+
+	if (toX === undefined || toY === undefined) 
+        return false;
+    
+    let k = 0;
+    while (k < decks) {
+        let i_x = x + k * kx;
+        let j_y = y + k * ky;
+        if(i_x == j_y || i_x == (9 - j_y)){
+            return false
+        }
+        k++;
+    }
+    for (let i = fromX; i < toX; i++) {
+        for (let j = fromY; j < toY; j++) {
+            if (field[j][i] == 1)
+                return false;
+        }
+    }
+	return true;
+}
+
+function getStartDeckCoordForDiagonalAlgoritm(decksNum, locationAlgorithm) {
+    let x, y;
+    let kx = getRandomNumber(1); 
+    let ky = (kx == 0) ? 1 : 0;
+
+    if (kx == 0) {
+		x = getRandomNumber(9); 
+        y = getRandomNumber(10 - decksNum);
+	} else {
+		x = getRandomNumber(10 - decksNum); 
+        y = getRandomNumber(9);
+	}
+
+    let result;
+    switch(SHIP_LOCATION_ALGORITHMS[locationAlgorithm]){
+        case 0:
+            result = checkShipLocationForRandomAlgoritm(x, y, kx, ky, decksNum);
+            break;
+        case 1:
+            result = checkShipLocationForDiagonalAlgoritm(x, y, kx, ky, decksNum);
+            break;
+    }
+    //let result = checkShipLocationForDiagonalAlgoritm(x, y, kx, ky, decksNum);
+
+	if (!result) 
+        return getStartDeckCoordForDiagonalAlgoritm(decksNum);
+    return {x, y, kx, ky}; // координаты первой палубы корабля, а также информацию о направлении
+}
+
+function getShipsLocation(locationAlgorithm) {
+    let SHIP_DATA = {
+		fourdeck: [1, 4],
+		tripledeck: [2, 3],
+		doubledeck: [3, 2],
+		singledeck: [4, 1]
+	};
+    for (let type in SHIP_DATA) {
+        let count = SHIP_DATA[type][0];
+		let decks = SHIP_DATA[type][1];
+        for (let i = 0; i < count; i++) {
+            let k = 0;
+			let shipInfo = getStartDeckCoordForDiagonalAlgoritm(decks, locationAlgorithm);
+            while (k < decks) {
+                let i_x = shipInfo.x + k * shipInfo.kx;
+                let j_y = shipInfo.y + k * shipInfo.ky;
+                if(i_x == j_y || i_x == (9 - j_y)){
+                    return getStartDeckCoordForDiagonalAlgoritm(decks, locationAlgorithm);
+                }
+                field[j_y][i_x] = 1;
+                k++;
+            }
+        }
+    }
+}
+
+chosenAlgorithm = 'diagonals_ship_location_algorithm';
+getShipsLocation(chosenAlgorithm);
+console.log(field);
+
+//let enemy = new Enemy(10);
+//randomLocationShips(enemy.playerField, enemy.ships);
 // for(let i = 0; i<10;i++){
 //     for(let j = 0;j<10;j++){
 //         let cur = enemy.playerField.getCell(i,j);
